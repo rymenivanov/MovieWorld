2813a383072ffc932c15d7ed3722d6a7007e4894
rymenivanov <rumen755@gmail.com>
2017-11-02 20:54:15 +0200

    Search fixed, button relocated, ng-stuff added

--- routes/actors.js
+++ routes/actors.js
@@ -2,15 +2,14 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
     console.log('123')
     var db = req.db;
-   
-    var movies = db.get('Movies');
-    movies.find({} , {} , function(err,docs){
+
+    var actors = db.get('Actors');
+    actors.find({}, {}, function (err, docs) {
         res.json(docs);
     })
-
 });
 
 module.exports = router;
