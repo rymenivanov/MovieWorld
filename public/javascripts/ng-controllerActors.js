a82a2caca4977f68212a12bc8d6bc7067152de97
rymenivanov <rumen755@gmail.com>
2017-11-05 19:07:05 +0200

    Flip cards for In Theaters and Actors

---
+++ public/javascripts/ng-controllerActors.js
@@ -0,0 +1,8 @@
+var app = angular.module("actorsApp", []);
+app.controller("allActors", function($scope, $http){
+
+    $http.get('/actors').then(function (data) {
+        $scope.actors = data.data;
+    });
+    
+})
--- public/javascripts/ng-controllerTemplates.js
+++ public/javascripts/ng-controllerTemplates.js
@@ -1,16 +1,18 @@
-var app = angular.module("myApp", ['ngRoute','moviesApp', 'inTheatersApp']);
+var app = angular.module("myApp", ['ngRoute','moviesApp', 'inTheatersApp', 'actorsApp']);
  
 app.config(function ($routeProvider) {
     $routeProvider
         .when('/home', {
-            templateUrl: 'views/home.htm',
-            
+            templateUrl: 'views/home.htm'     
         })
         .when('/moviestmpl', {
             templateUrl: 'views/moviesTmpl.htm'
         })
         .when('/intheaters', {
             templateUrl: 'views/inTheaters.htm'
+        })
+        .when('/allactors', {
+            templateUrl: 'views/allActors.htm'
         })
 });
 
